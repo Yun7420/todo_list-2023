95483705f80ee3b3c607bceed9eafe09ddc48082
hsy7420 <hsy742010@gmail.com>
2023-11-14 16:19:26 +0900

    03.초기데이터 생성

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -7,6 +7,26 @@ import FilterButton from "../ui/FilterButton";
 import Form from "../ui/Form";
 import TodoList from "../ui/TodoList";
 
+// 초기 데이터 생성
+const seedData = () => {
+  const seed = [
+    { id: "list1", name: "HTML 학습하기", completed: false },
+    { id: "list2", name: "CSS 학습하기", completed: false },
+    { id: "list3", name: "JAVASCRIPT 학습하기", completed: false },
+    { id: "list4", name: "REACT 학습하기", completed: false },
+  ];
+
+  setData(seed);
+};
+
+const setData = (seed) => {
+  localStorage.setItem("todoList", JSON.stringify(seed));
+};
+
+if(!JSON.parse(localStorage.getItem("todoList"))) {
+  seedData();
+}
+
 const Main = () => {
   return <Title></Title>;
 };
